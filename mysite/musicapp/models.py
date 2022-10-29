d08806cdf2174b961c07c80d6aec8411f060c448
BlaiseCJ <ezeblaise@gmail.com>
2022-10-29 03:02:38 +0100

    CRUD

--- mysite/musicapp/models.py
+++ mysite/musicapp/models.py
@@ -1,3 +1,24 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length = 300)
+    last_name = models.CharField(max_length = 300)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    Artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 500)
+    date_released = models.DateField(default = datetime.today)
+    likes = models.IntegerField()
+    artist_id = models.CharField(max_length = 500)
+
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.CharField(max_length = 2000)
+    song_id = models.IntegerField()
+
+
